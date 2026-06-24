acfa71b76dd2809effb1337d59bf635162b0781f
Bassam Mohamed <bm4747509@gmail.com>
2026-06-25 01:13:41 +0300

    Checkout UI

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -3,24 +3,36 @@ import { createContext, useContext, useState } from "react";
 // CONTEXT
 const CartContext = createContext();
 
+// DATA
+import { getProductById } from "../data/products";
+
 export default function CartProvider({ children }) {
 
     const [cartItems, setCartItems] = useState([]);
 
+    // ADD PRODUCT TO CART
     function addToCart(productID) {
         const exsitingItem = cartItems.find((item) => item.id === productID);
 
         if (exsitingItem) {
-            const updateCartItems = cartItems.map((item) => item.id === productID ? { id: productID, quantity: exsitingItem.quantity+ 1 } : item); // thid will be if item is existing
+            const updateCartItems = cartItems.map((item) => item.id === productID ? { id: productID, quantity: exsitingItem.quantity + 1 } : item); // thid will be if item is existing
             setCartItems(updateCartItems);
         } else {
             setCartItems([...cartItems, { id: productID, quantity: 1 }]); // this will be for the first time
         }
     }
 
+    // GET PRODUCT IN CART
+    function getCartItemsWithProducts() {
+        return cartItems.map((item) => ({
+            ...item,
+            product: getProductById(item.id)
+        })).filter(item => item.product)
+    }
+
 
     return (
-        <CartContext.Provider value={{ cartItems, addToCart }}>{children}</CartContext.Provider>
+        <CartContext.Provider value={{ cartItems, addToCart , getCartItemsWithProducts }}>{children}</CartContext.Provider>
     )
 }
 
--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,6 +1,40 @@
+// CONTEXT
+import { useCart } from '../context/CartContext';
+
+
 const Checkout = () => {
+    const { getCartItemsWithProducts } = useCart();
+    const cartItems = getCartItemsWithProducts();
+
     return (
-        <div>checkout Page</div>
+        <div className='page'>
+            <div className="container">
+                <h1 className="page-title">CheckOut</h1>
+                <div className="checkout-container">
+                    <div className="checkout-items">
+                        <h2 className="checkout-section-title">Order Summary</h2>
+                        {cartItems.map((item) => (
+                            <div className="checkout-item">
+                                <img src={item.product.image} alt={item.product.name} className="checkout-item-image" />
+                                <div className="checkout-item-details">
+                                    <h3 className="checkout-item-name">{item.product.name}</h3>
+                                    <p className="chechout-item-price">${item.product.price} each</p>
+                                </div>
+                                <div className="checkout-item-controls">
+                                    <div className="quantity-controls">
+                                        <button className="quantity-btn">-</button>
+                                        <span className="quantity-value">{item.quantity}</span>
+                                        <button className="quantity-btn">+</button>
+                                    </div>
+                                    <p className="checkout-item-total">${item.product.price * item.quantity}</p>
+                                    <button className="btn btn-secondary btn-small">Remove</button>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
